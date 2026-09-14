bc720f583d1787846b91caec36a710920ecd8a4e
swagerfemboy-sketch <swagerfemboy@gmail.com>
2026-09-14 08:40:47 -0300

    Cliente controller completo

--- src/main/java/com/redtel/controldepagos/controller/ClienteController.java
+++ src/main/java/com/redtel/controldepagos/controller/ClienteController.java
@@ -1,5 +1,30 @@
 package com.redtel.controldepagos.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.redtel.controldepagos.IService.IClienteService;
+import com.redtel.controldepagos.entidades.Cliente;
+
+@RestController
+@RequestMapping("/api/clientes")
 public class ClienteController {
-    
+    @Autowired
+    private IClienteService cService;
+
+    @GetMapping
+    public List<Cliente> getAllClientes() {
+        return cService.findAllClientes();
+    }
+
+    @PostMapping
+    public Cliente createCliente(@RequestBody Cliente cliente) {
+        return cService.saveCliente(cliente);
+    }
 }
